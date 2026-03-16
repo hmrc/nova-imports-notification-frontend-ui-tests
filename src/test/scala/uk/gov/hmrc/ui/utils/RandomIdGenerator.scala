7c59e86a3eae33ad065f8a5d6535c0bffdb472e8
ardurojaye <101007648+adurojaye@users.noreply.github.com>
2026-03-16 08:56:40 +0000

    review changes

--- src/test/scala/uk/gov/hmrc/ui/utils/RandomIdGenerator.scala
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmrc.ui.utils
-
-import scala.util.Random
-
-object RandomIdGenerator {
-  def generateRandomIdentifierValue(): String = Random.alphanumeric.take(6).mkString
-  def generateRandomVIN(): String             = Random.alphanumeric.take(17).mkString
-}
