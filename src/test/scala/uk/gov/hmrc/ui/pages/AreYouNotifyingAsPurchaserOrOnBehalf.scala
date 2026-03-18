f12cccf101b678500c14fff1c1df98680ec4d7f3
ardurojaye <101007648+adurojaye@users.noreply.github.com>
2026-03-18 13:54:19 +0000

    POM for IQ2 screen in-progress

---
+++ src/test/scala/uk/gov/hmrc/ui/pages/AreYouNotifyingAsPurchaserOrOnBehalf.scala
@@ -0,0 +1,29 @@
+package uk.gov.hmrc.ui.pages
+
+import org.openqa.selenium.By
+
+object AreYouNotifyingAsPurchaserOrOnBehalf extends BasePage {
+
+  override val pageUrl: String = s"$baseUrl/purchaser-or-on-behalf"
+
+  object PageLocators {
+    val purchaser: By = By.id("notifierType")
+    val onBehalf: By  = By.id("notifierType-2")
+  }
+
+  def verifyPageDisplayed(): Unit =
+    validatePage(
+      expectedUrl = pageUrl,
+      expectedHeading = "Are you notifying as the purchaser, or on behalf of a purchaser?"
+    )
+
+  def selectPurchaserAndContinue(): Unit = {
+    click(PageLocators.purchaser)
+    click(Locators.continueButton)
+  }
+
+  def selectOnBehalfAndContinue(): Unit = {
+    click(PageLocators.onBehalf)
+    click(Locators.continueButton)
+  }
+}
