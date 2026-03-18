3078c7053b6de0e037ac1a5dcd4ecab1a0aacd52
KezzaTezza99 <63915795+KezzaTezza99@users.noreply.github.com>
2026-03-18 10:10:27 +0000

    Refactor

--- src/test/scala/uk/gov/hmrc/ui/journeys/IndividualUserJourney.scala
+++ src/test/scala/uk/gov/hmrc/ui/journeys/IndividualUserJourney.scala
@@ -4,13 +4,12 @@ import uk.gov.hmrc.ui.helpers.AffinityGroup
 import uk.gov.hmrc.ui.pages.{AuthLoginPage, BasePage, VehicleBroughtIntoNIFromEUPage, VehicleBroughtIntoNIFromOutsideEUPage}
 
 object IndividualUserJourney extends BasePage {
-  override val pageUrl: String = ""
-
+  override val pageUrl: String              = "" // Needed due to extending BasePage
   def completeIndividualJourneyFlow(): Unit = {
     // TODO: This would eventually go page to page
   }
 
-  def userNeedsToImportAVehicle(): Unit = {
+  def userNeedsToImportAVehicleFromOutsideEU(): Unit = {
     AuthLoginPage.login(AffinityGroup.Individual)
     VehicleBroughtIntoNIFromEUPage.verifyPageDisplayed()
     selectNoAndContinue()
--- src/test/scala/uk/gov/hmrc/ui/specs/IndividualUserSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/IndividualUserSpec.scala
@@ -3,10 +3,13 @@ package uk.gov.hmrc.ui.specs
 import uk.gov.hmrc.ui.journeys.IndividualUserJourney
 
 class IndividualUserSpec extends BaseSpec {
-  Feature("Individual E2E User Journey") {
-    Scenario("Individual user imports vehicle to NI from EU and needs to import a vehicle") {
-      When("the user answers 'no' on IQ1 and needs to make an import declaration instead")
-      IndividualUserJourney.userNeedsToImportAVehicle()
+  Feature("Individual User needs to make an import instead of using NoVA service") {
+    Scenario("Individual user imports vehicle to NI from outside of the EU") {
+      When(
+        "the user answers 'no' for 'Are you completing a notification for a vehicle brought into Northern Ireland from an EU country?'"
+      )
+      Then("the user is directed to make an import declaration instead")
+      IndividualUserJourney.userNeedsToImportAVehicleFromOutsideEU()
     }
   }
 }
