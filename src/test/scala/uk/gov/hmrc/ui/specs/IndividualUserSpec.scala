2b3b36a837ff11ae205052b810defffa32aa7867
KezzaTezza99 <63915795+KezzaTezza99@users.noreply.github.com>
2026-03-19 17:08:44 +0000

    Adding to the journey and spec

--- src/test/scala/uk/gov/hmrc/ui/specs/IndividualUserSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/IndividualUserSpec.scala
@@ -3,6 +3,30 @@ package uk.gov.hmrc.ui.specs
 import uk.gov.hmrc.ui.journeys.IndividualUserJourney
 
 class IndividualUserSpec extends BaseSpec {
+  Feature("Individual User who is a 'Private Individual' and the 'Purchaser'"){
+    Scenario("A user completing an import as a private individual and as the purchaser"){
+      When("the user sets out to import a vehicle as the buyer and as a private individual")
+      Then("the user successfully imports a vehicle into NI")
+      IndividualUserJourney.userStartsJourneyAsAPrivateIndividualAndAsThePurchaser()
+    }
+  }
+
+  Feature("Individual User who is a 'Private Individual' and importing on behalf of a 'Registered Business'") {
+    Scenario("A user completing an import as a private individual on behalf of a business") {
+      When("the user sets out to import a vehicle as a private individual on behalf of a business")
+      Then("the user successfully imports a vehicle into NI")
+      IndividualUserJourney.userStartsJourneyAsAPrivateIndividualAndOnBehalfOfPurchaserThatIsARegisteredBusiness()
+    }
+  }
+
+  Feature("Individual User who is a 'Private Individual' on behalf of a 'Private Individual'") {
+    Scenario("A user completing an import as a private individual on behalf of a private individual") {
+      When("the user sets out to import a vehicle on the behalf of a private individual")
+      Then("the user successfully imports a vehicle into NI")
+      IndividualUserJourney.userStartsJourneyAsAPrivateIndividualAndOnBehalfOfPurchaserThatIsAPrivateIndividual()
+    }
+  }
+
   Feature("Individual User needs to make an import instead of using NoVA service") {
     Scenario("Individual user imports vehicle to NI from outside of the EU") {
       When(
