a63aeed5eee803ec32fc9adb366de461d5479a42
Kyle <63915795+KezzaTezza99@users.noreply.github.com>
2026-03-20 09:43:36 +0000

    Feature/adding pages to spec (#12)
    * Adding to the journey and spec
    
    * Formatting

--- src/test/scala/uk/gov/hmrc/ui/journeys/IndividualUserJourney.scala
+++ src/test/scala/uk/gov/hmrc/ui/journeys/IndividualUserJourney.scala
@@ -1,19 +1,52 @@
 package uk.gov.hmrc.ui.journeys
 
 import uk.gov.hmrc.ui.helpers.AffinityGroup
-import uk.gov.hmrc.ui.pages.{AuthLoginPage, VehicleBroughtIntoNIFromEUPage, VehicleBroughtIntoNIFromOutsideEUPage}
+import uk.gov.hmrc.ui.pages.{AreYouABusinessOrPrivateIndividual, AreYouNotifyingAsPurchaserOrOnBehalf, AuthLoginPage, PurchaserOnBehalfOfABusinessOrIndividual, VehicleBroughtIntoNIFromEUPage, VehicleBroughtIntoNIFromOutsideEUPage}
 
 object IndividualUserJourney {
-  def userCompletesFlowWithManualEntryOfAVehicle(): Unit = {
-    // TODO: This would eventually go page to page
+  def userStartsJourneyAsAPrivateIndividualAndAsThePurchaser(): Unit = {
+    AuthLoginPage.login(AffinityGroup.Individual)
+    VehicleBroughtIntoNIFromEUPage.verifyPageDisplayed()
+    VehicleBroughtIntoNIFromEUPage.selectYesAndContinue()
+    AreYouABusinessOrPrivateIndividual.verifyPageDisplayed()
+    // TODO: THIS WILL CHANGE FROM NO TO OPTION TWO / AS PRIVATE INDIVIDUAL
+    AreYouABusinessOrPrivateIndividual.selectNoAndContinue()
+    AreYouNotifyingAsPurchaserOrOnBehalf.verifyPageDisplayed()
+    // TODO: THIS WILL CHANGE FROM YES TO OPTION ONE / AS THE PURCHASER
+    AreYouNotifyingAsPurchaserOrOnBehalf.selectYesAndContinue()
+    // TODO: NOW ON SUMMARY PAGE
   }
 
-  def userCompletesFlowUpToSpreadsheetUpload(): Unit = {
-    // TODO:
+  def userStartsJourneyAsAPrivateIndividualAndOnBehalfOfPurchaserThatIsARegisteredBusiness(): Unit = {
+    AuthLoginPage.login(AffinityGroup.Individual)
+    VehicleBroughtIntoNIFromEUPage.verifyPageDisplayed()
+    VehicleBroughtIntoNIFromEUPage.selectYesAndContinue()
+    AreYouABusinessOrPrivateIndividual.verifyPageDisplayed()
+    // TODO: THIS WILL CHANGE FROM NO TO OPTION TWO / AS PRIVATE INDIVIDUAL
+    AreYouABusinessOrPrivateIndividual.selectNoAndContinue()
+    AreYouNotifyingAsPurchaserOrOnBehalf.verifyPageDisplayed()
+    // TODO: THIS WILL CHANGE FROM NO TO OPTION TWO / ON BEHALF OF PURCHASER
+    AreYouNotifyingAsPurchaserOrOnBehalf.selectNoAndContinue()
+    PurchaserOnBehalfOfABusinessOrIndividual.verifyPageDisplayed()
+    // TODO: THIS WILL CHANGE FROM YES TO OPTION ONE / NON-VAT REGISTERED BUSINESS
+    PurchaserOnBehalfOfABusinessOrIndividual.selectYesAndContinue()
+    // TODO: NOW ON SUMMARY PAGE
   }
 
-  def userCompletesFlowAfterSpreadsheetUpload(): Unit = {
-    // TODO:
+  def userStartsJourneyAsAPrivateIndividualAndOnBehalfOfPurchaserThatIsAPrivateIndividual(): Unit = {
+    AuthLoginPage.login(AffinityGroup.Individual)
+    VehicleBroughtIntoNIFromEUPage.verifyPageDisplayed()
+    VehicleBroughtIntoNIFromEUPage.selectYesAndContinue()
+    AreYouABusinessOrPrivateIndividual.verifyPageDisplayed()
+    // TODO: THIS WILL CHANGE FROM NO TO OPTION TWO / AS PRIVATE INDIVIDUAL
+    AreYouABusinessOrPrivateIndividual.selectNoAndContinue()
+    AreYouNotifyingAsPurchaserOrOnBehalf.verifyPageDisplayed()
+    // TODO: THIS WILL CHANGE FROM NO TO OPTION TWO / ON BEHALF OF PURCHASER
+    AreYouNotifyingAsPurchaserOrOnBehalf.selectNoAndContinue()
+    PurchaserOnBehalfOfABusinessOrIndividual.verifyPageDisplayed()
+    // TODO: THIS WILL CHANGE FROM NO TO OPTION TWO / NON-VAT REGISTERED PRIVATE INDIVIDUAL
+    PurchaserOnBehalfOfABusinessOrIndividual.selectNoAndContinue()
+    // TODO: NOW ON SUMMARY PAGE
   }
 
   def userNeedsToImportAVehicleFromOutsideEU(): Unit = {
