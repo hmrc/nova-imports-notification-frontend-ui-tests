b815d0d29d68db881c177368c6bca1a6c82543d2
KezzaTezza99 <63915795+KezzaTezza99@users.noreply.github.com>
2026-07-08 17:00:52 +0100

    Test data added

--- src/test/scala/uk/gov/hmrc/ui/data/TestData.scala
+++ src/test/scala/uk/gov/hmrc/ui/data/TestData.scala
@@ -1,11 +1,35 @@
 package uk.gov.hmrc.ui.data
 
+import uk.gov.hmrc.ui.models.UserDetails.{BusinessDetails, IndividualUserDetails}
+import uk.gov.hmrc.ui.models.{Notifier, Supplier}
+
 object TestData {
   object NotifierDetails {
-
+    val notifier = Notifier.NotifierDetails(
+      userDetails = IndividualUserDetails(
+        title = "Mr",
+        firstName = "John",
+        lastName = "Smith"
+      ),
+      phoneNumber = None,
+      mobileNumber = Some("07700 900999"),
+      email = "john.smith@example.com"
+    )
   }
 
   object SupplierDetails {
+    val businessSupplier = Supplier.BusinessSupplier(
+      userDetails = BusinessDetails(
+        businessName = "Test Company Ltd"
+      )
+    )
 
+    val individualSupplier = Supplier.IndividualSupplier(
+      userDetails = IndividualUserDetails(
+        title = "Mrs",
+        firstName = "Jane",
+        lastName = "Smith"
+      )
+    )
   }
 }
