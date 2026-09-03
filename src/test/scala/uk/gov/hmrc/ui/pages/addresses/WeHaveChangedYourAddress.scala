d3fb4c25b4df3d3dd5743c5fa42e930decc83626
KezzaTezza99 <63915795+KezzaTezza99@users.noreply.github.com>
2026-09-03 07:33:31 +0100

    Fomratting

--- src/test/scala/uk/gov/hmrc/ui/pages/addresses/WeHaveChangedYourAddress.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/addresses/WeHaveChangedYourAddress.scala
@@ -19,11 +19,10 @@ package uk.gov.hmrc.ui.pages.addresses
 import uk.gov.hmrc.ui.helpers.AddressPages
 import uk.gov.hmrc.ui.pages.BasePage
 
-/**
-   Notifier and Purchaser will only ever have one address but in theory we could have 100 different supplier addresses
-   So we need to decide what supplier number we want to provide. We will make the assumption it will only ever be 1 and
-   default this value however we can override this if we want to provide more.
- */
+/** Notifier and Purchaser will only ever have one address but in theory we could have 100 different supplier addresses
+  * So we need to decide what supplier number we want to provide. We will make the assumption it will only ever be 1 and
+  * default this value however we can override this if we want to provide more.
+  */
 class WeHaveChangedYourAddress(addressJourney: AddressPages, supplierNumber: Int = 1) extends BasePage {
   override val pageUrl: String = s"$baseUrl/${addressJourney.getWeHaveChangedYourAddressUrl(supplierNumber)}"
 
